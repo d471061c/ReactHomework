b5c3b57b8935f4b2302cb31699baa9ae798a5e8f
d471061c <cpastethat@gmail.com>
2018-01-16 20:28:31 +0200

    1.4 oliot taulukkoon

---
+++ Viikko_1/kurssivalikko_1_4/src/index.js
@@ -0,0 +1,64 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+const Otsikko = (props) => {
+    return (
+        <h1> {props.teksti} </h1>
+    )
+}
+
+const Osa = (props) => {
+    return (
+        <p>
+            <strong> {props.nimi}</strong>: {props.tehtavia}
+        </p>
+    )
+}
+
+const Sisalto = (props) => {
+    return (
+        <div>
+            <Osa nimi={props.osat[0].nimi} tehtavia={props.osat[0].tehtavia}/>
+            <Osa nimi={props.osat[1].nimi} tehtavia={props.osat[1].tehtavia}/>
+            <Osa nimi={props.osat[2].nimi} tehtavia={props.osat[2].tehtavia}/>
+        </div>
+    )
+}
+
+const Yhteensa = (props) => {
+    let summa = props.osat.map((osa) => (osa.tehtavia)).reduce((a, b) => a + b, 0);
+    return (
+        <p> Yhteensä: {summa} </p>
+    )
+}
+
+const App = () => {
+    const kurssi = 'Half Stack -sovelluskehitys'
+    const osat = [
+        {
+            nimi: 'Reactin perusteet',
+            tehtavia: 10,
+        },
+        {
+            nimi: 'Tiedonvälitys propseilla',
+            tehtavia: 7
+        },
+        {
+            nimi: 'Komponenttien tila',
+            tehtavia: 14
+        }
+    ]
+  
+    return (
+      <div>
+        <Otsikko teksti={kurssi}/>
+        <Sisalto osat={osat}/>
+        <Yhteensa osat={osat}/>
+      </div>
+    )
+  }
+  
+  ReactDOM.render(
+    <App />,
+    document.getElementById('root')
+  )
