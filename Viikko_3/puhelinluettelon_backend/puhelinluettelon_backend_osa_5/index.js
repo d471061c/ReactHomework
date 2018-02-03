a97153820251b04b71e31d1593ef19b4979e324b
d471061c <cpastethat@gmail.com>
2018-02-03 22:01:15 +0200

    Fix typo

--- Viikko_3/puhelinluettelon_backend/puhelinluettelon_backend_osa_5/index.js
+++ Viikko_3/puhelinluettelon_backend/puhelinluettelon_backend_osa_5/index.js
@@ -41,7 +41,7 @@ app.get("/api/persons/:id", (req, res) => {
 app.delete("/api/persons/:id", (req, res) => {
   const id = Number(req.params.id)
   persons = persons.filter(person => person.id !== id)
-  res-status(204).end()
+  res.status(204).end()
 })
 
 app.post("/api/persons", (req, res) => {
