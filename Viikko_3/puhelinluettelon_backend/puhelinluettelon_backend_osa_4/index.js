cb318a350c1d36f0aad0bc19c062f9da2e2e9b0c
d471061c <cpastethat@gmail.com>
2018-02-03 17:37:57 +0200

    Add status code 204

--- Viikko_3/puhelinluettelon_backend/puhelinluettelon_backend_osa_4/index.js
+++ Viikko_3/puhelinluettelon_backend/puhelinluettelon_backend_osa_4/index.js
@@ -39,7 +39,7 @@ app.get("/api/persons/:id", (req, res) => {
 app.delete("/api/persons/:id", (req, res) => {
   const id = Number(req.params.id)
   persons = persons.filter(person => person.id !== id)
-  res.end()
+  res.status(204).end()
 })
 
 app.get("/api/persons", (req, res) => {
