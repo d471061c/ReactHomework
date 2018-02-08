79fa8fdcad7b34b33b1b22cb8a5f2ac1d5b938c1
d471061c <cpastethat@gmail.com>
2018-02-08 17:49:57 +0200

    3.19* puhelinluettelo ja tietokanta, osa 7

---
+++ Viikko_3/puhelinluettelon_ja_tietokanta/puhelinluettelo_ja_tietokanta_osa_7/index.js
@@ -0,0 +1,89 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+const morgan = require('morgan')
+const cors = require('cors')
+const mongoose = require('mongoose')
+const Person = require('./models/Person')
+
+const url = 'mongodb://<username>:<person>@ds125288.mlab.com:25288/sandbox'
+mongoose.connect(url)
+
+morgan.token('body', function getBody(req) { return JSON.stringify(req.body) })
+
+const app = express()
+
+app.use(morgan(':method :url :body :res[content-length] :status - :response-time ms'))
+app.use(bodyParser.json())
+app.use(cors())
+app.use(express.static("build"))
+
+app.get("/api/persons", (req, res) => {
+  Person
+    .find({})
+    .then(result => {
+      res.json(result.map(Person.format));
+    })
+})
+
+app.get("/api/persons/:id", (req, res) => {
+  const id = req.params.id
+  Person.findById(id)
+    .then(result => {
+      res.json(result)
+    }).catch(error => {
+      res.status(404).json({ error: 'person not found' })
+    })
+})
+
+app.delete("/api/persons/:id", (req, res) => {
+  const id = req.params.id
+  Person.findByIdAndRemove(id)
+    .then(res.status(204).end())
+    .catch(error => console.log(error))
+})
+
+app.post("/api/persons", (req, res) => {
+  const person = req.body
+
+  if (person === undefined) {
+    return res.status(400).json({ error: 'Person missing' })
+  }
+
+  const personObj = new Person({
+    name: person.name,
+    number: person.number
+  })
+
+  personObj
+    .save()
+    .then(response => {
+      res.json(Person.format(response));
+    })
+    .catch(err => {
+      return res.status(400).json({ error: err.message })
+    })
+
+})
+
+app.put('/api/persons/:id', (req, res) => {
+  const id = req.params.id
+  const person = req.body
+
+  Person.findByIdAndUpdate(person.id, person)
+    .then(response => {
+      res.json(Person.format(response))
+    })
+    .catch(error => {
+      console.log(error);
+    })
+})
+
+app.get("/info", (req, res) => {
+  let content = `<p>puhelinluettelossa on ${persons.length} henkilön tiedot</p><p>${new Date()}</p>`
+  res.send(content)
+})
+
+const PORT = process.env.PORT || 3001;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
